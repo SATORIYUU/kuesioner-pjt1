a57492f7513381c5c8a3249d8604d28ea72ca47f
satrio akbar <satrioakbararadhana@gmail.com>
2026-07-16 08:00:39 +0700

    fix UI

--- app/Pages/admin/login.vue
+++ app/Pages/admin/login.vue
@@ -88,9 +88,9 @@
               class="w-full bg-[#004B87] hover:bg-sky-900 text-white font-bold py-3.5 px-6 rounded-xl flex items-center justify-center space-x-2 transition-all shadow-md text-sm sm:text-base cursor-pointer"
             >
               <span>Masuk Ke Dashboard</span>
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 sm:h-5 sm:w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 11-6 0v-1" />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-4 w-4 transform group-hover:translate-x-1 transition-transform duration-200">
+           <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
             </button>
           </div>
         </form>
