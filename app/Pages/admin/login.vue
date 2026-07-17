62667ee3e44e76ac739f6848f943bd53122b2ef8
satrio akbar <satrioakbararadhana@gmail.com>
2026-07-17 09:38:29 +0700

    button fix

--- app/Pages/admin/login.vue
+++ app/Pages/admin/login.vue
@@ -87,7 +87,7 @@
               type="submit" 
               class="w-full bg-[#004B87] hover:bg-sky-900 text-white font-bold py-3.5 px-6 rounded-xl flex items-center justify-center space-x-2 transition-all shadow-md text-sm sm:text-base cursor-pointer"
             >
-              <span>Masuk Ke Dashboard</span>
+              <span>Login</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="h-4 w-4 transform group-hover:translate-x-1 transition-transform duration-200">
            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
         </svg>
