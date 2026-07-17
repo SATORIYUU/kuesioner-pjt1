03ab02bea5622046dfc85ebbb900233b623840ce
satrio akbar <satrioakbararadhana@gmail.com>
2026-07-17 09:38:44 +0700

    button fix

--- app/Pages/admin/register.vue
+++ app/Pages/admin/register.vue
@@ -106,7 +106,7 @@
               type="submit" 
               class="w-full bg-[#004B87] hover:bg-sky-900 text-white font-bold py-3 px-6 rounded-xl flex items-center justify-center space-x-2 transition-all shadow-md text-sm cursor-pointer"
             >
-              <span>Submit Registration Request</span>
+              <span>Submit</span>
               <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
               </svg>
